048206e491ad24580b94e73100b8e0a22bdb438e
Anthony Oberto <anthonyoberto@gmail.com>
2015-06-29 16:49:11 -0700

    began functionality for navigation messages, featured section navigation, completed functionality for pinboard sort by functionality. Issue with pinboard audio player.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,38 @@
-console.log('\'Allo \'Allo!');
+
+
+$(document).ready(function() {
+
+	// NAV ACTIVITY
+	$('nav li').click(function() {
+		$('nav li').not(this).removeClass('current');
+	    
+	    if($(this).hasClass('current')){
+	    	return false;
+	    }
+		
+		$(this).toggleClass('current');
+	});
+
+
+	// MESSAGES DROPDOWN
+	$('#messages').click(function() {
+		$('#messages-dropdown').toggleClass('hidden');
+	});
+
+
+	//FEATURED IMAGE CHANGE
+	$('.featured-category').click(function() {
+		$('.featured-category').not(this).removeClass('active');
+	    
+	    if($(this).hasClass('active')){
+	    	return false;
+	    }
+		
+		$(this).toggleClass('active');
+	});
+
+	$(function(){
+	    $('#Container').mixItUp();  
+	});
+
+});
